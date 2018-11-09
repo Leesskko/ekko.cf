f9129380ce0dd0b987fcf52e537e43045504d01f
IV66\Ekko <ekko@888mkt.com>
2018-11-09 08:07:10 +0800

    撒地方

--- app/Http/Websocket/WebsocketHandler.php
+++ app/Http/Websocket/WebsocketHandler.php
@@ -25,23 +25,8 @@ class WebsocketHandler implements HandlerContract
      */
     public function onOpen($fd, Request $request)
     {
-        if (!$request->input('sid')) {
-            $payload = json_encode([
-                'sid' => base64_encode(uniqid()),
-                'upgrades' => [],
-                'pingInterval' => Config::get('swoole_websocket.ping_interval'),
-                'pingTimeout' => Config::get('swoole_websocket.ping_timeout')
-            ]);
-            $initPayload = Packet::OPEN . $payload;
-            $connectPayload = Packet::MESSAGE . Packet::CONNECT;
-
-            App::make('swoole.server')->push($fd, $initPayload);
-            App::make('swoole.server')->push($fd, $connectPayload);
-
-            return true;
-        }
-
-        return false;
+        App::make('swoole.server')->push($fd, $request->user());
+        return true;
     }
 
     /**
